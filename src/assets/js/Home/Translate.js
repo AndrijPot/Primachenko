b66b002255f4f7341075819bdce0c8c9abf2de64
Andrij <Andrijpotapov1@gmail.com>
2024-05-12 21:40:00 +0300

    text for button

--- src/assets/js/Home/Translate.js
+++ src/assets/js/Home/Translate.js
@@ -7,7 +7,7 @@ toggleButton.addEventListener('click', function() {
     texts.forEach(function(text) {
         if (isEnglish) {
             text.textContent = text.getAttribute('data-uk');
-            toggleButton.textContent = 'Змінити на англійську';
+            toggleButton.textContent = 'Change to english';
         } else {
             text.textContent = text.getAttribute('data-en');
             toggleButton.textContent = 'Змінити на українську';
